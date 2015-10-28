2dc1ce974a54d435c3a434d1819811aa8ef02222
Krzysztof Polanski <krzysztof_polanski@o2.pl>
2015-10-28 13:52:04 +0000

    Reverting to Python 2.7 because craziness

--- wellington/scripts/wellington_bootstrap_wrapper.sh
+++ wellington/scripts/wellington_bootstrap_wrapper.sh
@@ -5,31 +5,31 @@
 samtools index $1
 samtools index $2
 #hard pass in out_treatment1.bed and out_treatment2.bed as destinations
-python3 ../scripts/wellington_bootstrap.py $@
+python ../scripts/wellington_bootstrap.py $@
 
 #unlike the previous analysis, now everything is handily placed in the root
 
 #do two visualisations, for each possible file
 mkdir treatment1_output_visualisation
 #get the wiggle tracks
-python3 ../scripts/dnase_wig_tracks.py $3 $1 treatment1_output_visualisation/fw_cuts.wig treatment1_output_visualisation/rv_cuts.wig
+python ../scripts/dnase_wig_tracks.py $3 $1 treatment1_output_visualisation/fw_cuts.wig treatment1_output_visualisation/rv_cuts.wig
 if [ -s out_treatment1.bed ]
 	then
 		#get the average profile thing
-		python3 ../scripts/dnase_average_profile.py out_treatment1.bed $1 treatment1_output_visualisation/average_footprint.png
+		python ../scripts/dnase_average_profile.py out_treatment1.bed $1 treatment1_output_visualisation/average_footprint.png
 		#get the heatmap
-		python3 ../scripts/dnase_to_javatreeview.py out_treatment1.bed $1 treatment1_output_visualisation/javatreeview_heatmap_ready.csv
+		python ../scripts/dnase_to_javatreeview.py out_treatment1.bed $1 treatment1_output_visualisation/javatreeview_heatmap_ready.csv
 fi
 mkdir treatment2_output_visualisation
 #get the wiggle tracks
-python3 ../scripts/dnase_wig_tracks.py $3 $2 treatment2_output_visualisation/fw_cuts.wig treatment2_output_visualisation/rv_cuts.wig
+python ../scripts/dnase_wig_tracks.py $3 $2 treatment2_output_visualisation/fw_cuts.wig treatment2_output_visualisation/rv_cuts.wig
 if [ -s out_treatment2.bed ]
 	then
 		#get the average profile thing
-		python3 ../scripts/dnase_average_profile.py out_treatment1.bed $2 treatment2_output_visualisation/average_footprint.png
+		python ../scripts/dnase_average_profile.py out_treatment1.bed $2 treatment2_output_visualisation/average_footprint.png
 		#get the heatmap
-		python3 ../scripts/dnase_to_javatreeview.py out_treatment1.bed $2 treatment2_output_visualisation/javatreeview_heatmap_ready.csv
+		python ../scripts/dnase_to_javatreeview.py out_treatment1.bed $2 treatment2_output_visualisation/javatreeview_heatmap_ready.csv
 fi
 
 #get the wiggle tracks
-python3 ../scripts/dnase_wig_tracks.py $1 $2 output_visualisation/fw_cuts.wig output_visualisation/rv_cuts.wig
+python ../scripts/dnase_wig_tracks.py $1 $2 output_visualisation/fw_cuts.wig output_visualisation/rv_cuts.wig
--- wellington/scripts/wellington_footprint_wrapper.sh
+++ wellington/scripts/wellington_footprint_wrapper.sh
@@ -5,7 +5,7 @@
 #we also need to run the indexing ourselves because yes
 mkdir analysis
 samtools index $2
-python3 ../scripts/wellington_footprints.py $@
+python ../scripts/wellington_footprints.py $@
 
 #move wellington run contents into main folder
 mv analysis/* .
@@ -13,10 +13,10 @@ rmdir analysis
 
 #get the average profile thing
 mkdir output_visualisation
-python3 ../scripts/dnase_average_profile.py WellingtonFootprints.FDR.bed $2 output_visualisation/average_footprint.png
+python ../scripts/dnase_average_profile.py WellingtonFootprints.FDR.bed $2 output_visualisation/average_footprint.png
 
 #get the heatmap
-python3 ../scripts/dnase_to_javatreeview.py WellingtonFootprints.FDR.bed $2 output_visualisation/javatreeview_heatmap_ready.csv
+python ../scripts/dnase_to_javatreeview.py WellingtonFootprints.FDR.bed $2 output_visualisation/javatreeview_heatmap_ready.csv
 
 #get the wiggle tracks
-python3 ../scripts/dnase_wig_tracks.py $1 $2 output_visualisation/fw_cuts.wig output_visualisation/rv_cuts.wig
+python ../scripts/dnase_wig_tracks.py $1 $2 output_visualisation/fw_cuts.wig output_visualisation/rv_cuts.wig
