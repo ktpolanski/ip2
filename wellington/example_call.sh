79584e91a0dd0553316b21edb83431797440ed49
Krzysztof Polanski <krzysztof_polanski@o2.pl>
2015-11-02 15:11:47 +0000

    This works locally

--- wellington/example_call.sh
+++ wellington/example_call.sh
@@ -2,3 +2,5 @@
 docker run -it --rm -v /home/kpolanski/docker_demo/testdata-k562:/agave wellington-footprint wgEncodeUwDgfK562Hotspots.broadPeak wgEncodeUwDgfK562Aln.bam
 
 docker run -it --rm -v /home/kpolanski/docker_demo/testdata-boot:/agave wellington-bootstrap wgEncodeUwDgfHepg2Hotspots.broadPeak wgEncodeUwDgfK562Hotspots.broadPeak --start wgEncodeUwDgfHepg2Aln.bam wgEncodeUwDgfK562Aln.bam
+
+docker run -it --rm -v /home/kpolanski/docker_demo/testdata-bootmini:/agave wellington-bootstrap ex_reg.bed --start ex1.bam ex2.bam
