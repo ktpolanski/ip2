7bad353960bef08d1a49f1954978305e19f3cf04
Krzysztof Polanski <krzysztof_polanski@o2.pl>
2015-11-26 12:10:56 +0000

    Enhanced functionality of indexing step

--- HMT_index/hmt_index_wrapper.sh
+++ HMT_index/hmt_index_wrapper.sh
@@ -9,6 +9,8 @@ set -e
 # $5 - MEME-friendly motif input
 # $6 - significance threshold
 # $7 - number of top motifs to take
+# $8 - toggle whether to do Uniprobe to MEME conversion
+# $9 - toggle whether to remove promoter overlapping bits with gene sequences
 
 #start off by filtering the .gff3 to gene lines only
 grep -P '\tgene\t' $2 > genelines.gff3
@@ -21,17 +23,30 @@ cut -f 1-2 $1.fai > bedgenome.genome
 python3 /scripts/parse_genelines.py $3
 #the python script takes the genelines.gff3 file and makes a genelines.bed out of it
 bedtools flank -l $4 -r 0 -s -i genelines.bed -g bedgenome.genome > promoters.bed
+#remove overlapping promoter chunks
+if [ $9 == '--NoOverlap' ]
+	then
+		bedtools subtract -a promoters.bed -b genelines.bed > promoters.bed
+fi
 bedtools getfasta -fi $1 -bed promoters.bed -fo promoters_rough.fa
 #this results in some really crappy nomenclature for gene names
 #so let's make promoters.fa ourselves
 python3 /scripts/parse_promoters.py
 
 #now we can actually FIMO our way to victory
-/root/meme/bin/fasta-get-markov promoters.fa >> promoters.bg
+/root/meme/bin/fasta-get-markov promoters.fa > promoters.bg
 #FIMO barfs ALL the output. that's not good. time for individual FIMOs
 #on individual MEME-friendly motif files too
 mkdir memefiles
-python3 /scripts/parse_memefile.py $5
+#optionally turn Uniprobe into MEME. MEME sucks as a format, Uniprobe is easier
+if [ $8 == '--Uniprobe' ]
+	then
+		/root/meme/bin/uniprobe2meme $5 > MEME-motifs.txt
+		python3 /scripts/parse_memefile.py MEME-motifs.txt
+	else
+		python3 /scripts/parse_memefile.py $5
+fi
+#now loop over all the individual motif files
 for fid in memefiles/*.txt
 do
 	/root/meme/bin/fimo --text --thresh $6 --verbosity 1 --bgfile promoters.bg $fid promoters.fa >> fimo.txt
