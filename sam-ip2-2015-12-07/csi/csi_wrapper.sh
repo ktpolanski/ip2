547ce83e91484a44be1c302d6d62b20e3faacc45
Krzysztof Polanski <krzysztof_polanski@o2.pl>
2016-03-04 11:18:25 +0000

    csi postprocess tweak

--- sam-ip2-2015-12-07/csi/csi_wrapper.sh
+++ sam-ip2-2015-12-07/csi/csi_wrapper.sh
@@ -8,3 +8,7 @@ cp -r /scripts/html/ html/
 
 #make webapp port of results
 python3 /scripts/csi/webapp-json.py csi_out.h5 > html/results.json
+
+#parse networks (MAP and marginal)
+python3 /scripts/csi/getnetwork.py --map csi_out.h5 > csi_MAP.csv
+python3 /scripts/csi/getnetwork.py --marginal csi_out.h5 > csi_marginal.csv
