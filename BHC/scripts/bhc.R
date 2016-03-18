6c1145a841d272f3fe09dbc3be290fac44072326
Krzysztof Polanski <krzysztof_polanski@o2.pl>
2016-03-18 12:55:06 +0000

    was editing wrong bhc.R

--- BHC/scripts/bhc.R
+++ BHC/scripts/bhc.R
@@ -1,12 +1,15 @@
 #!/usr/bin/Rscript
 
-suppressPackageStartupMessages(require(argparse))
-suppressPackageStartupMessages(require(BHC))
+library(argparse)
+library(RColorBrewer)
+library(BHC)
 
 #argument parsing
 parser = ArgumentParser()
 parser$add_argument('file', nargs=1, help='CSV with gene expression, first column gene names, first row time points')
+parser$add_argument('--Pool', dest='pool', type='integer', default=1, help='Number of threads for BHC parallelisation. Default: 1')
 parser$add_argument('--Mode', dest='mode', default='mutinomial', help='Mode of operation (multinomial/time-course). Default: multinomial')
+parser$add_argument('--MakeHeatmap', dest='heatmap', action='store_true', help='Flag. If specified, a heatmap of the resulting clustering will be produced')
 args = parser$parse_args()
 
 #basic data prep
@@ -15,8 +18,38 @@ genes = rownames(data)
 samples = colnames(data)
 data = data.matrix(data)
 
+#data discretisation
+percentiles = FindOptimalBinning(data, genes, transposeData=TRUE, verbose=TRUE)
+discreteData = DiscretiseData(t(data), percentiles=percentiles)
+discreteData = t(discreteData)
+
+#BHC runs proper
 if (args$mode == 'time-course')
 {
 	#get them time points as time points
-	samples = as.integer(samples)
+	samples = as.numeric(samples)
+	#clustering proper
+	hc = bhc(discreteData, genes, timePoints=samples, dataType='time-course', numThreads=args$pool, verbose=TRUE)
+	if (args$heatmap)
+	{
+		png('heatmap.png')
+		heatmap(data, Colv=NA, Rowv=hc, scale="none", col=brewer.pal(11,'RdBu'))
+		dev.off()
+	}
+} else {
+	hc = bhc(discreteData, genes, dataType='multinomial', numThreads=args$pool, verbose=TRUE)
+	if (args$heatmap)
+	{
+		#cluster samples too, for the heatmap
+		percentiles2 = FindOptimalBinning(data, genes, transposeData=FALSE, verbose=TRUE)
+		discreteData2 = DiscretiseData(data, percentiles=percentiles2)
+		discreteData2 = t(discreteData2)
+		hc2 = bhc(discreteData2, samples, dataType='multinomial', numThreads=args$pool, verbose=TRUE)
+		png('heatmap.png')
+		heatmap(data, Colv=hc2, Rowv=hc, scale="none", col=brewer.pal(11,'RdBu'))
+		dev.off()
+	}
 }
+
+#write out clusters
+WriteOutClusterLabels(hc, 'clusters.txt', verbose=TRUE)
