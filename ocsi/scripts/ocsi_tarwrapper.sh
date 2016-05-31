76f66bd9bf7790e82950271b776424a5aefec7b7
Krzysztof Polanski <krzysztof_polanski@o2.pl>
2016-05-31 15:19:56 +0100

    cyversewarwick compatibility changes

--- ocsi/scripts/ocsi_tarwrapper.sh
+++ ocsi/scripts/ocsi_tarwrapper.sh
@@ -6,7 +6,7 @@ touch tempfile
 sleep 5
 
 #run thing
-python3 /scripts/ocsi2.py "${@:1}"
+python /scripts/ocsi2.py "${@:1}"
 
 #wrap up output and kick out tempfile
 find . -mindepth 1 -newer tempfile -exec tar -rf FullOutput.tar {} \;
